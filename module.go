c367fe7033979ddb0920f87926b09e15d91065c1
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-07 01:24:15 +0530

    handle course_id query param in the get handler

--- module.go
+++ module.go
@@ -9,10 +9,10 @@ import (
 
 // Module is a slice of lessons with some meta data
 type Module struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	CourseID    int    `json:"courseID"`
+	ID          int    `json:"id,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	CourseID    int    `json:"courseID,omitempty"`
 }
 
 func createModule(c echo.Context) error {
@@ -35,6 +35,30 @@ func createModule(c echo.Context) error {
 
 func getModules(c echo.Context) error {
 	var mod []Module = make([]Module, 0)
+
+	mid := c.QueryParam("course_id")
+	if len(mid) != 0 {
+		courseID, err := strconv.Atoi(mid)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "malformatted course_id"})
+		}
+
+		rows, err := db.Query("SELECT id, title FROM module WHERE course_id=?", courseID)
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			m := Module{}
+			rows.Scan(&m.ID, &m.Title)
+			mod = append(mod, m)
+		}
+
+		return c.JSON(http.StatusOK, mod)
+
+	}
+
 	row, err := db.Query("SELECT id, title, course_id FROM module")
 	if err != nil {
 		return err
