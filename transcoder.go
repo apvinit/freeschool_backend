d366698f2664cbcfc6a8035e1a6e174539aba68a
h4rdw1r3 <vinaugust6@gmail.com>
2020-06-13 23:24:59 +0530

    update transcode video resolution to 480p

--- transcoder.go
+++ transcoder.go
@@ -14,7 +14,7 @@ func transcodeToHLS(fileName string) {
 		"ffmpeg",
 		"-i", "media/"+fileName,
 		"-profile:v", "baseline",
-		"-level", "3.0", "-s", "640x360",
+		"-level", "3.0", "-s", "854x480",
 		"-start_number", "0",
 		"-hls_time", "10", "-hls_list_size", "0",
 		"-f", "hls", "transcoded/"+fileName+"/index.m3u8").Output()
